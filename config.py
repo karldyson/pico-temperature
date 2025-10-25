a713c2f7d6a95b2b85d7c9b0f9af7105fcc7e1b3
Karl Dyson <code@karld.uk>
2025-10-26 00:00:52 +0100

    Added UDP multicast and a crude sensor calibration offset method
    * Added UDP multicast
    * Added sensor offset to facilitate a crude method of calibrating sensor offset

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 SSID = "wifi-ssid-goes-here"
 PSK = "wifi-password-goes-here"
 GPIO_PIN = 15
+UDP_PORT = 9999
+MCAST_GROUP = "239.0.0.1"
 SLEEP = 10
 BASE_URL = "http://iotplotter.com/api/v2/feed/"
 API_KEY = "api-key-goes-here"
@@ -8,6 +10,6 @@ FEED_ID = "feed-id-goes-here"
 FEED_ENABLED = False
 
 SENSOR = {}
-SENSOR["default"] = {"name": "DefaultSensorName"}
+SENSOR["default"] = {"name": "DefaultSensorName", "offset": 0.0}
 SENSOR["0000000000000001"] = {"name": "SomeSensorName"}
 SENSOR["0000000000000002"] = {"name": "SomeOtherSensorName"}
--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from itk_pico.wifi import WiFi
 from itk_pico.logger import Logger
 from time import sleep
 import json
+import socket
 import config
 import requests
 
@@ -30,16 +31,20 @@ for sensor in sensor_config:
     # set them each to the default
     if "default" in config.SENSOR.keys():
         sensor_config[sensor]["name"] = config.SENSOR["default"]["name"]
+        sensor_config[sensor]["offset"] = config.SENSOR["default"]["offset"]
     else:
         sensor_config[sensor]["name"] = "DefaultSensor" + sensor_counter
+        sensor_config[sensor]["offset"] = 0.0
         sensor_counter += 1
 
     # if the sensor has specific settings in the config, use those
     if sensor in config.SENSOR.keys():
         if "name" in config.SENSOR[sensor].keys():
             sensor_config[sensor]["name"] = config.SENSOR[sensor]["name"]
+        if "offset" in config.SENSOR[sensor].keys():
+            sensor_config[sensor]["offset"] = config.SENSOR[sensor]["offset"]
 
-    Logger.print(f"Sensor {sensor}; Name: {sensor_config[sensor]['name']}") 
+    Logger.print(f"Sensor {sensor}; Name: {sensor_config[sensor]['name']}; Offset: {sensor_config[sensor]['offset']}")
 
 # Initialise the WiFi
 wifi = WiFi()
@@ -63,7 +68,7 @@ Logger.print("Starting main loop...")
 while True:
     # Check we're still connected to the wifi...
     wifi.try_reconnect_if_lost()
-    
+
     # Loop through the sensors getting the current temperatures
     sensors = temperature_sensor.get_temperature()
 
@@ -73,20 +78,43 @@ while True:
 
     # Loop through each of the sensors
     for sensor in sensors:
-        
-        # get the friendly name and the fudge factor from the config
-        sensor_name = sensor_config[sensor]["name"]
 
-        # apply the fudge factor to the received temperature value
+        # get the friendly name and the offset factor from the config
+        sensor_name = sensor_config[sensor]["name"]
+        sensor_offset = sensor_config[sensor]["offset"]
+
+        # apply the offset factor to the received temperature value
         temperature = sensors[sensor]
+        temperature = temperature + sensor_offset
 
         # ...some debug output
-        Logger.print(f"Sensor: {sensor}; Name: {sensor_name}; Temp: {temperature}")
+        Logger.print(f"Sensor: {sensor}; Name: {sensor_name}; Temp: {temperature}; offset: {sensor_offset}")
+
+        # create the UDP message dictionary
+        message_dict = {"name": sensor_name, "sensor": sensor, "temperature": temperature, "offset": sensor_offset}
+
+        # ...and encode it into JSON
+        message = json.dumps(message_dict)
+
+        # create the UDP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        # ...and turn it into a multicast packet
+        if hasattr(socket, "IP_MULTICAST_TTL"):
+            sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 1)
+
+        # send the packet to the network
+        sock.sendto(message, (config.MCAST_GROUP, config.UDP_PORT))
+        Logger.print(f"sent multicast: {message}")
+
+        # ...and close the socket
+        sock.close()
+        # end of multicast
 
         # add to API payload
         payload["data"][sensor_name] = []
         payload["data"][sensor_name].append({"value": sensors[sensor]})
-    
+
     # if the feed is enabled, send the payload to the API endpoint
     if config.FEED_ENABLED:
         response = requests.post(feed_url, headers=headers, data=json.dumps(payload))
